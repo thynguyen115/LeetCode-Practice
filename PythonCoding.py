1095435d042e6e7d10870012d152d1fcc97c78e2
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-07-04 12:23:48 -0700

    Update PythonCoding.py

--- PythonCoding.py
+++ PythonCoding.py
@@ -1,12 +1,26 @@
+""" Problem 1: Swap Case """
+def swap_case(s):
+    returned_str = ""
+    for char in s:
+        if char.isalpha():
+            if char.isupper():
+                returned_str += char.lower()
+            else:
+                returned_str += char.upper()
+        else:
+            returned_str += char    
+    return returned_str
+  
 if __name__ == '__main__':
-  """ Problem 1: Print hash code of a tuple (t) of an input tuple of n integers separated by space """
+  """ Problem 2: Print hash code of a tuple (t) of an input tuple of n integers separated by space """
     n = int(input())
     integer_list = map(int, input().split())
     t = tuple(integer_list)
     print(hash(t))
   
-  """ Problem 2: Given an input dictionary {"student_name" : [score1, score2, score3]}
-      Print average scores of a student (note: give 2 decimal places)) 
+  """
+   Problem 3: Given an input dictionary {"student_name" : [score1, score2, score3]}
+   Print average scores of a student (note: give 2 decimal places)) 
   """
   n = int(input())
     student_marks = {}
@@ -18,3 +32,4 @@ if __name__ == '__main__':
     stu_scores = student_marks[query_name]
     avg = sum(stu_scores) / len(stu_scores)
     print("{:0.2f}".format(avg))
+
