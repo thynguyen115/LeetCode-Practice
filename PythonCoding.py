170f81872fa91c4a299455a755a25246f7ed1819
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-07-04 12:17:35 -0700

    Update PythonCoding.py

--- PythonCoding.py
+++ PythonCoding.py
@@ -4,3 +4,17 @@ if __name__ == '__main__':
     integer_list = map(int, input().split())
     t = tuple(integer_list)
     print(hash(t))
+  
+  """ Problem 2: Given an input dictionary {"student_name" : [score1, score2, score3]}
+      Print average scores of a student (note: give 2 decimal places)) 
+  """
+  n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    stu_scores = student_marks[query_name]
+    avg = sum(stu_scores) / len(stu_scores)
+    print("{:0.2f}".format(avg))
