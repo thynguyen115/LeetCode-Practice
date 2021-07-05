d7275fb9f432e8fc1af60f3063ed69db4e0d7b77
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-07-05 10:34:16 -0700

    Update PythonCoding_HackerRank.py

--- PythonCoding_HackerRank.py
+++ PythonCoding_HackerRank.py
@@ -52,7 +52,7 @@ def count_substring(string, sub_string):
         return 1 + count_substring(string[1:], sub_string)
     else:
         return count_substring(string[1:], sub_string)
-  
+    
 if __name__ == '__main__':
   """ Problem 6: Print hash code of a tuple (t) of an input tuple of n integers separated by space """
     n = int(input())
@@ -97,3 +97,55 @@ if __name__ == '__main__':
             lst[4] = True
      for i in range(5):
         print(lst[i])
+
+""" Problem 9: print logo of HackerRank 
+    H    
+   HHH   
+  HHHHH  
+ HHHHHHH 
+HHHHHHHHH
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHHHHHHHHHHHHHHHHHHHHHH   
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+  HHHHH               HHHHH             
+                    HHHHHHHHH 
+                     HHHHHHH  
+                      HHHHH   
+                       HHH    
+                        H 
+"""
+#Replace all ______ with rjust, ljust or center. 
+
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
+#Middle Belt
+for i in range((thickness+1)//2):
+    print((c*thickness*5).center(thickness*6))    
+
+#Bottom Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))    
+
+#Bottom Cone
+for i in range(thickness):
+    print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
