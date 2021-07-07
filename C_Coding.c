1fb296be0bb365557269878cdf2cef2f2fb9bf19
thynguyen115 <77253286+thynguyen115@users.noreply.github.com>
2021-07-07 00:23:20 -0700

    Learn C

--- C_Coding.c
+++ C_Coding.c
@@ -5,4 +5,12 @@
 int main(void) {
     int num = 42;
     printf("The answer is %d.\n", num);
+    char* greeting = "Hello";
+    printf("%s", greeting);
+    char greet[10] = "Hello!";
+    printf("\n%c", greet[3]);
+    float int_division = 9 / 4; // give 2.0 due to 9, 4 are integers
+    float float_division = 9.0 / 4; // gives 2.25
+    printf("\n%f", float_division);
+    printf("\n%f", int_division);
 }
